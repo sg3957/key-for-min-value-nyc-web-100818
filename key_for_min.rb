1c7fa74715da3b4f05bfea7255af5e35f3cf76e0
Sydney Grant <sg3957@nyu.edu>
2018-09-30 03:26:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -19,14 +19,15 @@
 # end
 
 def key_for_min_value(name_hash)
+  if name_hash == {}
+    return nil
+  else
 value1 = 500
 key1 = ""
 name_hash.each do |key, value|
   if value < value1
     value1 = value
     key1 = key
-elsif name_hash == {}
-  return nil
 end
 end
 return key1
